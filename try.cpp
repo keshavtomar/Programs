0fcd99408eee3e1fceecb48eb0f9c71ef0f01159
keshav <tomarkeshavofficial@gmail.com>
2022-08-21 00:29:18 +0530

    try.cpp added to gitignore

--- try.cpp
+++ try.cpp
@@ -1,28 +1,2 @@
-#include <iostream>
-#include <climits>
-#include <algorithm>
-#include <vector>
-#include <list>
-#include <cmath>
-#include <cstring>
-#include <set>
-#include <map>
 
-#define FOR(a, b, c) for (int a = b; a < c; a++)
-#define ll long long int
-#define pb push_back
-#define pp pop_back
-#define vi vector<int>
-#define all(v) v.begin(),v.end()
 
-using namespace std;
-
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-    }
-    return 0;
-}
